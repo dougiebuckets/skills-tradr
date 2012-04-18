129a13a418fbd87a326a1c40663865776522d0bf
doug@crowdrouser.com <doug@crowdrouser.com>
2012-04-18 18:27:24 -0400

    initial commit with RoR directory stuff & README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkillsTradr::Application.config.secret_token = 'f5ff3649a8d1ca08f21a5822a958164bf79ec68333b80b025c991c8c3697d5919d68a2dbafef58c0bd86e04f7452363ecb0452033908cd0a5f09a9e7193c6db3'
