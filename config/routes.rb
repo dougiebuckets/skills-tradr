ffbfddb8734b99247732645ef8e96e3896eed5b5
rosslazer <rosslazer@gmail.com>
2012-04-22 12:16:00 -0400

    Fixed the rows in views

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ root :to => "home#index"
 
 
   devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
-
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
